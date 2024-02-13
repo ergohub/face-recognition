92234fbe7db21203496333c07612f92d8ba9f407
Derek Stanley <nebari@dereksmudio1387.home>
2024-02-13 20:57:34 +0000

    Updated loop with map to highlight multiple faces

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,20 +1,20 @@
-import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
-    // box.map = (boxes) => {
-        
-    // }
+const FaceRecognition = ({ imageUrl, boxes }) => {
+
     return(
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputImage' alt='face for processing' src={ imageUrl } width='500px' height='auto' />
-                <div className='boundingBox'style={{ top:box.topRow, left:box.leftCol, right:box.rightCol, bottom:box.bottomRow }}>
-                </div>
-                
+                    {
+                        boxes.map((box, id) => (
+                            <div key={id} className='boundingBox'style={{ top:box.topRow, left:box.leftCol, right:box.rightCol, bottom:box.bottomRow }}></div>
+                        ))
+                    }
             </div>
         </div>
     );
+    
 }
 
 export default FaceRecognition;
